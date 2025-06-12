0fc931d55bf57ff8d2a5c04e4fda4f66c47c0c4e
Pemmana Sowmya <147087128+sowmya1p@users.noreply.github.com>
2025-06-12 13:45:42 +0530

    Time: 0 ms (100%), Space: 44.7 MB (38.41%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -13,31 +13,18 @@ class Solution {
         if(head==null||head.next==null){
             return head;
         }
-        ListNode h1=new ListNode(-1);
-        ListNode h2=new ListNode(-1);
-        ListNode t1=head;
-        ListNode t2=head.next;
-        ListNode tt1=h1;
-        tt1.next=new ListNode(t1.val);
-        tt1=tt1.next;
-        ListNode tt2=h2;
-        tt2.next=new ListNode(t2.val);
-        tt2=tt2.next;
-        while(t1.next!=null&&t1.next.next!=null){
-            tt1.next=new ListNode(t1.next.next.val);
-            tt1=tt1.next;
-            t1=t1.next.next;
+        ListNode even=head.next;
+        ListNode evenHead=head.next;
+        ListNode odd=head;
+        ListNode oddHead=head;
+        while(even!=null&&even.next!=null){
+            odd.next=odd.next.next;
+            even.next=even.next.next;
+           // odd.next=odd.next.next;
+            even=even.next;
+            odd=odd.next;
         }
-        while(t2.next!=null&&t2.next.next!=null){
-            tt2.next=new ListNode(t2.next.next.val);
-            tt2=tt2.next;
-            t2=t2.next.next;
-        }
-        ListNode temp=h1.next;
-        while(temp.next!=null){
-           temp=temp.next;
-        }
-        temp.next=h2.next;
-        return h1.next;
+        odd.next=evenHead;
+        return oddHead;
     }
 }
